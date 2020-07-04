947e49aaa7b9deac6f9d383a442d701810d85042
Carlos Cesar Valle <carloscesarvalle@Carloss-iMac.local>
2020-07-04 15:48:11 -0400

    first iteration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import contacts from './contacts.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    showingContacts: [...contacts].splice(0,5),
+    restOfContacts: [...contacts].splice(5),
+  }
+
+showContact = () => {
+  let contactList = this.state.showingContacts.map((eachContact, id)=>{
+        return <tr key={eachContact.id}>
+          <th><img style={{width:"80px"}} src={eachContact.pictureUrl} alt={eachContact.name}></img></th>
+          <th>{eachContact.name}</th> 
+          <th>{eachContact.popularity}</th>
+        </tr>
+  })
+  return contactList
+}
+
+
+
+  render() {
+    return (
+      <div>
+        <table>
+          <thead>
+          <tr>
+            <th>Pictures</th>
+            <th>Name</th> 
+            <th>Popularity</th>
+          </tr>
+          </thead>
+        <tbody>
+          {this.showContact()}
+        </tbody>
+        </table>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
